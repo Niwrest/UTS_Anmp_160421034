b9348cbe3de517c85164ff8cbad3cd812dc39f53
ROG STRIX <s160421034@student.ubaya.ac.id>
2024-04-20 16:14:37 +0700

    Commit 1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UTS_Hobby_160421034"
+include(":app")
+ 
