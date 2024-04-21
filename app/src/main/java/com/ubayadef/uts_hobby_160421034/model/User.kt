3c8dd391462f34dc51b55a9a05613039726a373b
ROG STRIX <s160421034@student.ubaya.ac.id>
2024-04-21 19:29:39 +0700

    com 4

--- app/src/main/java/com/ubayadef/uts_hobby_160421034/model/User.kt
+++ app/src/main/java/com/ubayadef/uts_hobby_160421034/model/User.kt
@@ -2,6 +2,7 @@ package com.ubayadef.uts_hobby_160421034.model
 
 data class User(
     val iduser: String,
+    val username: String,
     val first_name: String,
     val last_name: String,
     val password: String,
